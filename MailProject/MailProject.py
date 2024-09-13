fc9a51796e159f507868f1e3e34cb9b43885b6da
ioo5959 <ioo22@pukyong.ac.kr>
2024-09-13 18:44:34 +0900

    인자값 전달

--- MailProject/MailProject.py
+++ MailProject/MailProject.py
@@ -2,9 +2,8 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
-from fileinput import filename
-
 import pandas as pd
+import sys
 
 
 def send_email(sender_email, receiver_email, app_password, subject, text, html):
@@ -17,15 +16,16 @@ def send_email(sender_email, receiver_email, app_password, subject, text, html):
     part1 = MIMEText(text, "plain")
     part2 = MIMEText(html, "html")
 
-    #첨부파일
-    file_name = '정보융합대학.xlsx'
-    with open(file_name, 'rb') as attach_file:
-        attachment = MIMEApplication(attach_file.read())
-        attachment.add_header('Content-Disposition', 'attachment', filename=file_name)
-
     message.attach(part1)
     message.attach(part2)
-    message.attach(attachment)
+
+    #첨부파일
+    for i in range(3, len(sys.argv)-3):
+        file_name = sys.argv[i]  # 첨부파일명
+        with open(file_name, 'rb') as attach_file:
+            attachment = MIMEApplication(attach_file.read())
+            attachment.add_header('Content-Disposition', 'attachment', filename=file_name)
+        message.attach(attachment)
 
     #이메일 서버 이용
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
@@ -36,18 +36,18 @@ def send_email(sender_email, receiver_email, app_password, subject, text, html):
 
 if __name__ == "__main__": #main함수
 
-    sender_email = "gomath72@gmail.com"
-    app_password = "iqny kqsm cukz prfj" #앱비밀번호
+    sender_email = sys.argv[1] #보내는 사람 메일
+    app_password = sys.argv[2] #앱 비밀번호
 
 
-    f = open('emailcontent.txt', 'r')  # 파일 열기
+    f = open('emailcontent.txt', 'r')  # 이메일 내용이 있는 텍스트 파일 열기
     subject = f.readline() #맨 처음 한 줄 제목
     text = f.read()
     f.close()
 
     html = f"<html><body><p>{text.replace('\n', '<br>')}</p></body></html>"  # html형식으로 본문 만들기
 
-    email_lis = pd.read_excel('test.xlsx', usecols=["이메일"])
+    email_lis = pd.read_excel('test.xlsx', usecols=["이메일"]) #메일 주소가 있는 엑셀 파일 접근
     for index, row in email_lis.iterrows(): #한 행씩 접근
         receiver_email = row["이메일"]
         send_email(sender_email, receiver_email, app_password, subject, text, html)
